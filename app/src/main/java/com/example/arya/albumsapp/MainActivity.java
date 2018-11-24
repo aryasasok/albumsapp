f0b1e2778a04dd17e465f249fe715573bae6fa64
Arya <arya@Aryas-MacBook-Air.local>
2018-11-24 16:03:26 -0500

    camera opening

--- app/src/main/java/com/example/arya/albumsapp/MainActivity.java
+++ app/src/main/java/com/example/arya/albumsapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.arya.albumsapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //This is testing
+    }
+
+    public void openCamera(View view) {
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        startActivity(intent);
     }
 }
